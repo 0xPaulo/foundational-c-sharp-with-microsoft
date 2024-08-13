9a636d6f3a6ea2a94ea106dc1922fb1aa49d31ee
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-08-13 15:34:31 -0300

    exercicio completo

---
+++ exercicios/arrays/manipular-matrizes.cs
@@ -0,0 +1,12 @@
+// System.Console.Clear();
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] cadaPedido = orderStream.Split(",");
+// Array.Sort(cadaPedido);
+// foreach (var item in cadaPedido)
+// {
+//     if (item.Length == 4)
+//     {
+//         System.Console.WriteLine(item);
+//     }
+//     else System.Console.WriteLine($"{item} \t- Error");
+// }
