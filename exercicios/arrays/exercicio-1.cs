e726846008a6708804cdc902ecad3db828df6b2f
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-08-13 15:10:06 -0300

    reorganizando diretorios

---
+++ exercicios/arrays/exercicio-1.cs
@@ -0,0 +1,18 @@
+// using System.Runtime.ConstrainedExecution;
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// // ehT kciuq nworb xof spmuj revo eht yzal god
+
+// string[] palavras = pangram.Split(" ");
+// string uniaoFrase = "";
+// string resultado = "";
+// for (int i = 0; i < palavras.Length; i++)
+// {
+//     char[] palavrasDivididas = palavras[i].ToCharArray();
+//     Array.Reverse(palavrasDivididas);
+//     uniaoFrase = String.Join("", palavrasDivididas);
+//     uniaoFrase += " ";
+//     resultado += uniaoFrase;
+// }
+// System.Console.WriteLine(resultado);
+
