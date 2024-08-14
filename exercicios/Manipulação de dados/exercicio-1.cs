05c3a4acabc6e68e83c67c232846ad8189fe5f06
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-08-14 16:50:09 -0300

    reorganizando diretorio

---
+++ exercicios/Manipulação de dados/exercicio-1.cs
@@ -0,0 +1,40 @@
+// Console.Clear();
+// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// // Your logic here
+// System.Console.WriteLine($"Dear {customerName}, \nAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\nCurrently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\nOur new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:N}.\n");
+
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// string comparisonMessage = "";
+
+// // Your logic here
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += $"{currentReturn:P}".PadRight(12);
+// comparisonMessage += $"{currentProfit:N}\n";
+// comparisonMessage += currentProduct.PadRight(20);
+// comparisonMessage += $"{newReturn:P}".PadRight(12);
+// comparisonMessage += $"{newProfit:N}\n";
+// Console.WriteLine(comparisonMessage);
+
+
+// // Dear Ms. Barros,
+// // As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+// // Currently, you own 2,975,000.00 shares at a return of 12.75%.
+
+// // Our new product, Glorious Future offers a return of 13.13%.  Given your current volume, your potential profit would be ¤63,000,000.00.
+
+// // Here's a quick comparison:
+
+// // Magic Yield         12.75%   $55,000,000.00      
+// // Glorious Future     13.13%   $63,000,000.00
---
+++ exercicios/Manipulação de dados/exercicio-2.cs
@@ -0,0 +1,108 @@
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1) break;
+
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+
+//     // Note the overload of the Substring to return only the remaining 
+//     // unprocessed message:
+//     message = message.Substring(closingPosition + 1);
+// }
+
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
+// char[] openSymbols = { '[', '{', '(' };
+// int startPosition = 5;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// // next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+
+// string data = "12345John Smith          5000  3  ";
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData); //123455000  3
+
+
+// const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+// string quantity = "";
+// string output = "";
+
+// // Your work here
+
+// string span = "<span>";
+// string div = "<div>";
+
+// int quantityInit = input.IndexOf(span) + span.Length;
+// int quantityEnd = input.LastIndexOf("</span>");
+// int quantityLengh = quantityEnd - quantityInit;
+// quantity = input.Substring(quantityInit, quantityLengh);
+
+// int divInit = input.IndexOf(div) + div.Length;
+// int divEnd = input.IndexOf("</div>");
+// int divLengh = divEnd - divInit;
+// output = input.Substring(divInit, divLengh);
+// output = output.Replace("&trade", "&reg");
+
+// Console.WriteLine(quantity);
+// Console.WriteLine(output);
+// // Quantity: 5000
+// // Output: <h2>Widgets &reg;</h2><span>5000</span>
