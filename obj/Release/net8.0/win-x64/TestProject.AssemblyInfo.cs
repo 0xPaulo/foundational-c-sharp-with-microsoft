f94c2b89a1e041a7698c3d2f921e940af07bf4cb
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-08-20 17:28:31 -0300

    atualizando projeto

--- obj/Release/net8.0/win-x64/TestProject.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/TestProject.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b31101edabeaf0d61b3a408b9390687485e69b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b55914a302e2b9f43555ab719caf1125361c995")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
